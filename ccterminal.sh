e118eb2b6b12ea7a9e0ea5ac76129fe898bf9a98
klinstifen <klinstifen@gmail.com>
2020-01-12 17:39:32 -0500

    Update to limit freeGear to once per day

--- ccterminal.sh
+++ ccterminal.sh
@@ -4,83 +4,104 @@ export PGPASSWORD=$PGPASSWD
 function getBalance(){ gear=$(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "SELECT gear FROM users WHERE username='$1'");}
 function getUsers(){ users=$(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "SELECT username FROM users");}
 function transferGear(){
-  $(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "UPDATE users SET gear = gear - $2 WHERE username = '$3'");
-  $(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "UPDATE users SET gear = gear + $2 WHERE username = '$1'");
-}
-function verifyUser(){ userFound=$(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "SELECT count(username) from users WHERE username='$1'");}
-function awardGear(){
+  result=$(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "UPDATE users SET gear = gear - $2 WHERE username = '$3'");
   result=$(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "UPDATE users SET gear = gear + $2 WHERE username = '$1'");
 }
+function verifyUser(){ userFound=$(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "SELECT count(username) from users WHERE username='$1'");}
+function checkLastGear(){
+  lastChecked=$(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "SELECT lastfreegear from users WHERE username = '$1'");
+}
+function awardFreeGear(){
+  result=$(psql -h $GHOST -U $GUSER -d $GTABLE -t -c "UPDATE users SET gear = gear + $2,lastfreegear = '$3' WHERE username = '$1'");
+}
 
+today=`date +%Y-%m-%d`
+function printC(){
+  content=$1
+  col=$(tput cols)
+  printf "%*s\n" $(((${#content}+$col)/2)) "$content"
+}
+
+function printP(){
+  content=$1
+  col=$(tput cols)
+  printf "%*s" $(((${#content}+$col)/2)) "$content"
+}
 
 function logo(){
-echo "  __                       .__              .__   "
-echo "_/  |_  ___________  _____ |__| ____ _____  |  |  "
-echo "\   __\/ __ \_  __ \/     \|  |/    \\__  \ |  |  "
-echo " |  | \  ___/|  | \/  Y Y  \  |   |  \/ __ \|  |__"
-echo " |__|  \___  >__|  |__|_|  /__|___|  (____  /____/"
-echo "           \/            \/        \/     \/      "
+printC "  __                       .__              .__   "
+printC "_/  |_  ___________  _____ |__| ____ _____  |  |  "
+printC "\   __\/ __ \_  __ \/     \|  |/    \\__  \ |  |  "
+printC " |  | \  ___/|  | \/  Y Y  \  |   |  \/ __ \|  |__"
+printC " |__|  \___  >__|  |__|_|  /__|___|  (____  /____/"
+printC "           \/            \/        \/     \/      "
 }
 
 function displayHeader(){
   clear
   columns=31
-  echo
+  tput cup 5
   #echo "   ***      TERMINAL      **   "
-  #logo
-  figlet "terminal"
-  echo
-  echo "          TechAhoy Inc         "
-  echo "         Ridgewood, NY         "
-  echo
-  echo "   ***                    ***  "
-  echo ""
-  echo "-------------------------------"
-  printf "%*s\n" $(((${#1}+$columns)/2)) "$1"
-  echo "-------------------------------"
-  echo ""
+  logo
+  #figlet "terminal"
+  #printC "this is a test"
+  printC ""
+  printC "          TechAhoy Inc         "
+  printC "         Ridgewood, NY         "
+  printC
+  printC "   ***                    ***  "
+  printC ""
+  printC "-------------------------------"
+  #printf "%*s\n" $(((${#1}+$columns)/2)) "$1"
+  printC "$1"
+  printC "-------------------------------"
+  printC ""
 }
 
 function displayFooter(){
-  echo "-------------------------------"
-  read -n 1 -p "Press any key to check your current balance..."
-
+  printC "-------------------------------"
+  printP "Press any key to check your current balance..."
+  read -n 1 
 }
 
 function displayAlert(){
   columns=31
-  printf "%*s\n" $(((${#1}+$columns)/2)) "$1"
-  echo ""
+  #printf "%*s\n" $(((${#1}+$columns)/2)) "$1"
+  printC "$1"
+  printC ""
 }
 
 function displayHelp(){
   displayHeader "HELP"
-  echo "Options:"
-  echo "'1' -> Get your current gear balance"
-  echo "'2' -> Send gear to another user"
-  echo "'3' -> Earn gear by completing the daily challenge"
-  echo "'h' -> Display this menu"
-  echo "'x' -> Quit the terminal"
-  echo ""
-  read -n 1 -p "Press any key to retun to the main menu..."
+  printC "Options:                                          "
+  printC "'1' -> Get your current gear balance              "
+  printC "'2' -> Send gear to another user                  "
+  printC "'3' -> Earn gear by completing the daily challenge"
+  printC "'h' -> Display this menu                          "
+  printC "'x' -> Quit the terminal                          "
+  printC ""
+  printP "Press any key to retun to the main menu..."
+  read -n 1
   return 0;
 }
 
 function displayMenu(){
   until [ "$ans" = "x"]; do
     displayHeader "MENU"
-    echo " 1. Check gear balance"
-    echo " 2. Send gear"
-    echo " 3. Daily Challenge"
-    echo ""
-    tput setaf 3; tput smso; echo " $. ** FREE GEAR **"; tput sgr0
-    echo ""
-    read -n 1 -p "Please enter your choice (h for help):" ans;
+    printC " 1. Check gear balance"
+    printC " 2. Send gear         "
+    printC " 3. Daily Challenge   "
+    printC ""
+    tput setaf 3
+    printC " $. ** FREE GEAR **   "
+    tput sgr0
+    printC ""
+    printP "Please enter your choice (h for help):"; read -n 1 ans;
     case $ans in
       1) clear; displayBalance ;;
       2) clear; sendGear ;;
       3) clear; dailyChallenge ;;
-      $) clear; freeGear ;;
+      $) clear; freeGearCheck ;;
       h|H) clear; displayHelp ;;
       x) clear; return 0 ;;
       *) clear ;;
@@ -94,103 +115,135 @@ function sendGear(){
   sgInit=0
   until [ "$sgInit" = "y" ];do
     displayHeader "SEND GEAR"
-    echo -n " Available Balance:";tput setaf 3;echo $gear"g";tput sgr0
-    echo
-    echo " List of users available:"
-    printf "%s\n" "$users"
-    echo
-    read -n 1 -p "Do you want to send gear? (y/n)" sgInit
+    printP "Available Balance:";tput setaf 3;echo $gear"g";tput sgr0
+    printC
+    printC "List of users available:"
+    for u in $users; do
+      printC $u
+    done
+    printC
+    printP "Do you want to send gear? (y/n)"
+    read -n 1 sgInit
     if [ "$sgInit" = "n" ]; then
       return 0;
     fi
   done
-  echo
-  read -p "Enter the username to whom you want to send gear:" sgUser;
-  read -p "Enter the amount of gear to send:" sgGear;
+  printC ""
+  printP "Enter the username to whom you want to send gear:"
+  read sgUser;
+  printP "Enter the amount of gear to send:                ";tput cub 16
+  read sgGear;
   verifyUser $sgUser
   if (("$sgGear" > "$gear")); then
-    tput cup 11 0
-    tput ed
-    displayAlert "*** ERROR ***"
-    echo " You do not have enough gear."
-    echo " You tried to send "$sgGear"g, but you only have"$gear"g!"
-    echo ""
-    read -n 1 -p "Press any key to return to the main menu..."
+    displayHeader "SEND GEAR - ERROR!"
+    #displayAlert "*** SEND GEAR - ERROR ***"
+    printC " You do not have enough gear."
+    printP " You tried to send "$sgGear"g, but you only have";echo $gear"g!"
+    printC ""
+    printP "Press any key to return to the main menu..."
+    read -n 1
     return 0;
   fi
   if ((!"$userFound")); then
-    tput cup 11 0
-    tput ed
-    displayAlert "*** ERROR ***"
-    echo " The user "$sgUser" does not exist!"
-    echo ""
-    read -n 1 -p "Press any key to return to the main menu..."
+    displayHeader "SEND GEAR - ERROR!"
+    #displayAlert "*** ERROR ***"
+    printC " The user $sgUser does not exist!"
+    printC ""
+    printP "Press any key to return to the main menu..."
+    read -n 1
     return 0;
   fi
   until [ "$sgConfirm" = "y" ];do
-    tput cup 11 0
-    tput ed
-    displayAlert "*** CAUTION ***"
-    echo -n 	" You are about to send ";tput setaf 3;echo $sgGear"g";tput srg0
-    echo " to "$sgUser"!"
-    echo
-    read -n 1 -p "Press 'y' to confirm or 'a' to abort:" sgConfirm;
+    displayHeader "SEND GEAR - CAUTION!"
+    #displayAlert "*** CAUTION ***"
+    printP "You are about to send ";tput setaf 3;echo $sgGear"g";tput sgr0
+    printC "to "$sgUser"!"
+    printC ""
+    printP "Press 'y' to confirm or 'a' to abort:"
+    read -n 1 sgConfirm;
     if [ "$sgConfirm" = "a" ]; then
       return 0;
     fi
   done
 
-  transferGear $sgUser $sgGear 'ganondorfcc'
+  transferGear $sgUser $sgGear $USER
   #sendGearNow
 }
 
 function displayBalance(){
   getBalance $USER
   displayHeader "GEAR BALANCE"
-  echo "  "`date`
-  echo ""
-  echo -n "  User: ";tput setaf 2;echo $USER;tput sgr0;
-  echo -n "  Available Balance: ";tput setaf 3;echo $gear"g";tput sgr0;
-  echo ""
-  echo ""
-  echo "  !!  Earn gear thru the    !!"
-  echo "  !!  the Daily Challenge!  !!"
-  echo ""
-  echo "-------------------------------"
-  echo ""
-  read -n 1 -p "Press any key to return to the main menu..."
+  printC "$today"
+  printC ""
+  printC "  User: $USER"
+  printP "  Available Balance: ";tput setaf 3;echo $gear"g";tput sgr0;
+  printC ""
+  printC ""
+  printC "  !!  Earn gear thru the    !!"
+  printC "  !!  the Daily Challenge!  !!"
+  printC ""
+  printC "-------------------------------"
+  printC ""
+  printP "Press any key to return to the main menu..."
+  read -n 1
   return 0;
 }
 
 function dailyChallenge(){
   displayHeader "DAILY CHALLENGE"
-  echo "Instructions:"
-  echo "Decode the following payload successfully and give the answer to an instructor."
-  echo ""
-  echo "Reward:"
-  tput setaf 3;echo "10g";tput sgr0
-  echo ""
-  echo "Payload:"
-  echo "Inaj rj ymj lifw!"
-  echo ""
-  read -n 1 -p "Press any key to return to the main menu..."
+  printC "Instructions:                            "
+  printC "Decode the following payload successfully"
+  printC "and give the answer to an instructor.    "
+  printC ""
+  printP "Reward: "
+  tput setaf 3
+  echo "10g                                      ";tput sgr0
+  printC ""
+  printC "Payload:                                 "
+  printC "Inaj rj ymj lifw!                        "
+  printC ""
+  printP "Press any key to return to the main menu..."
+  read -n 1
   return 0;
 }
 
 function freeGear(){
   displayHeader "FREE GEAR"
   freeAmt=$((1 + RANDOM % 10))
-  echo "  "`date`
-  echo ""
-  echo -n "Congrats you have earned ";tput setaf 3;echo $freeAmt"g";tput sgr0
-  echo ""
-  echo "Login again tomorrow for more free gear!"
-  echo ""
-  awardGear $USER $freeAmt
-  echo "-------------------------------"
-  read -n 1 -p "Press any key to check your current balance..."
+  printC ""
+  printC "$today"
+  printC ""
+  printP "Congrats you have earned "
+  tput setaf 3;echo $freeAmt"g";tput sgr0
+  printC ""
+  printC "Login again tomorrow for more free gear!"
+  printC ""
+  awardFreeGear $USER $freeAmt $today
+  printC "-------------------------------"
+  printC "Press any key to check your current balance..."
+  read -n 1
   displayBalance
+}
+
+function freeGearCheck(){
+  checkLastGear $USER
+  if [ -z $lastChecked ];then
+    freeGear
+  elif [ `date -d $lastChecked +%s` -lt `date -d $today +%s` ];then
+    freeGear
+  else
+    displayHeader "FREE GEAR"
+    printC "Last Checked: $lastChecked"
+    printC ""
+    printC "You already earned your free gear."
+    printC "Try again tomorrow!"
+    printC ""
+    printC "-------------------------------"
+    printC "Press any key to return to the main menu..."
+    read -n 1
+  fi
 }
 
 displayMenu
 clear
+
