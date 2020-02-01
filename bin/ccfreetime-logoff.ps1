74d1137e0a3ae4c55d284fdb805838045f161a95
klinstifen <klinstifen@gmail.com>
2020-02-01 16:55:37 -0500

    Update to utilize DB record of gear balance

--- bin/ccfreetime-logoff.ps1
+++ bin/ccfreetime-logoff.ps1
@@ -2,44 +2,74 @@
 $currentUser = $env:UserName
 $currentBalance = 0
 $conn = New-Object -comobject ADODB.Connection
-
+$today = $(get-date).toString("MM/dd/yyyy")
 # use existing 64 bit ODBC System DSN that we set up manually
 $conn.Open("CCTerminal")
 
-# Connect to DB and retrieve user stats
-$recordset = $conn.Execute("SELECT * FROM users WHERE username = '$currentUser'")
-if ($recordset.EOF -ne $True) { #Ensure user is in database
-  $loginTime = Get-WmiObject win32_networkloginprofile | ? {$_.lastlogon -ne $null} | % {[Management.ManagementDateTimeConverter]::ToDateTime($_.lastlogon)}
-  $userFriendlyName = #The person's first name
-  $currentBalance = $recordset.Fields.Item("gear").value #Current gear balance (1 gear = 1 minute)
-  echo "current balance: $currentBalance"
-  #Max freetime (currently 30 mintes)
-  if ($currentBalance -gt 30){
-      $freeTime = 30
+function updateGearBal(){
+  $maxFreeTime = 30 #query this from DB in the future
+  $recordset = $conn.Execute("Update users set lastGearBalUpdate = '$today' WHERE username = '$currentUser'")
+  $recordset = $conn.Execute("SELECT gear,freetimeGearBal FROM users WHERE username='$currentUser'")
+  $userGear = $recordset.Fields.Item("gear").value
+  $userFreeTimeGearBal = $recordset.Fields.Item("freetimeGearBal").value
+  if($userFreeTimeGearBal.Equals([DBNull]::Value)){$userFreeTimeGearBal = 0} #check for null
+  $freeTimeDif = ($maxFreeTime - $userFreeTimeGearBal)
+  if ($userGear -gt $freeTimeDif){
+    $recordset = $conn.Execute("Update users set freetimeGearBal = '$maxFreeTime' WHERE username = '$currentUser'")
+    $recordset = $conn.Execute("Update users set gear = gear - '$freeTimeDif' WHERE username = '$currentUser'")
+    write-host "max freetime!"
+    return $maxFreeTime
   } else {
-      $freeTime = $currentBalance
+    $recordset = $conn.Execute("Update users set freetimeGearBal = freetimeGearBal + '$freeTimeDif' WHERE username = '$currentUser'")
+    $recordset = $conn.Execute("Update users set gear = 0 WHERE username = '$currentUser'")
+    write-host "some freetime"
+    return ($userFreeTimeGearBal + $freeTimeDif)
   }
 }
-$timeSpan = New-TimeSpan -Minutes $freeTime #Convert gear balance into a time object
-$totalTime = $loginTime + $timeSpan #Compute logoff time by adding gear balance to logon time
 
-while ($freeTime -ge 0){
-  if ($(get-date) -gt $totalTime) { #You outta time - time to logoff
-    $str = "logoff"
-    Invoke-Expression $str
-  } elseif ($(($totalTime - $(get-date)).Minutes) -lt 5) {
-    #Give a warning
-    $remainingTime = $currentBalance
+# Connect to DB and verify freetime
+while (1) {
+  $recordset = $conn.Execute("SELECT * FROM stats")
+  if ($recordset.EOF -ne $True) { #Ensure we have results
+      $freetime = $recordset.Fields.Item("freetime").value #freetime = 1
+      if($freetime -eq 1){
+        # Alert user
+        #$wshell = New-Object -ComObject Wscript.Shell
+        #$wshell.Popup("Freetime has begun!`nThe computer will logoff when your time expires.",0,"codeCadet Freetime",0x30)
+        # Connect to DB and retrieve user stats
+        $recordset = $conn.Execute("SELECT * FROM users WHERE username = '$currentUser'")
+        if ($recordset.EOF -ne $True) { #Ensure user is in database
+          $lastGearBalUpdate = $recordset.Fields.Item("lastGearBalUpdate").value
+          $freetimeGearBal = $recordset.Fields.Item("freetimeGearBal").value
+          write-host $freetimeGearBal
+          if($lastGearBalUpdate.Equals([DBNull]::Value)){$freetimeGearBal = updateGearBal} #check for null
+          if ($lastGearBalUpdate -lt $today){
+            $freetimeGearBal = updateGearBal
+          }
+          $freetimeGearBal = $freetimeGearBal - 1
+          $loginTime = Get-WmiObject win32_networkloginprofile | ? {$_.lastlogon -ne $null} | % {[Management.ManagementDateTimeConverter]::ToDateTime($_.lastlogon)}
+          $userFriendlyName = #The person's first name
 
-    $wshell = New-Object -ComObject Wscript.Shell
+          $timeSpan = New-TimeSpan -Minutes $freetimeGearBal #Convert gear balance into a time object
+          $totalTime = $loginTime + $timeSpan #Compute logoff time by adding gear balance to logon time
 
-    $wshell.Popup("You have $freeTime minutes of free time remaining.",0,"Time is almost up!",0x30)
-
-  }
-  else {
-    #Have fun!
+          if ($(get-date) -gt $totalTime) { #You outta time - time to logoff
+            $str = "logoff"
+            #Invoke-Expression $str
+          } elseif ($(($totalTime - $(get-date)).Minutes) -lt 5) {
+            #Give a warning
+            $wshell = New-Object -ComObject Wscript.Shell
+            $wshell.Popup("You have $freetimeGearBal minutes of free time remaining.",0,"Time is almost up!",0x30)
+          }
+          else {
+            #Have fun!
+          }
+          #update freetimeGearBal
+          $recordset = $conn.Execute("Update users set freetimeGearBal = '$freetimeGearBal' WHERE username = '$currentUser'")
+        } else {
+          write-host "oops shouldn't be here"
+        }
+      }
   }
   Start-Sleep -s 60 #sleep for one minute
-  $freeTime -= 1 #Subtract a minute
-  $recordset = $conn.Execute("Update users set gear = gear - 1 WHERE username = '$currentUser'")
 }
